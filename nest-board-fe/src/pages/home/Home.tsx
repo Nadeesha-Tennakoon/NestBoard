e661aaef11deb81e3467cf1e1ab56c19334fb284
Nadeesha-Tennakoon <ntennakoon48@gmail.com>
2026-06-04 23:32:34 +0530

    Frontend/Property Card with styles

--- nest-board-fe/src/pages/home/Home.tsx
+++ nest-board-fe/src/pages/home/Home.tsx
@@ -1,9 +1,11 @@
+import { PropertyCard } from '@/components/common/PropertyCard'
 import { HeroSection } from './components/HeroSection'
 
 export function Home() {
   return (
     <>
       <HeroSection />
+      <PropertyCard />
     </>
   )
 }
